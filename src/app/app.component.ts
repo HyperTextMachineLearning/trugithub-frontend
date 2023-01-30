196e69729bce304b6c36b660f1a0aba7fcb5a30f
Ritik Matere <dev.ritik.matere@gmail.com>
2023-01-31 02:07:13 +0530

    Rewrite: index.html and app-component
    index.html had app-root which inturn had router-outlet; navigating home when pressing on brand wasn't possible.
    Hence now complete <body> is in app-root & within it is router-outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
 	title = 'trugit';
 	
-	
+	constructor(private router: Router){}
+
+	goToLanding () {
+		this.router.navigate(['/'])
+	}
 }

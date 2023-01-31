1bf2730e87cf29d09f270a72dbee846e066a84b5
Ritik Matere <dev.ritik.matere@gmail.com>
2023-01-31 19:17:23 +0530

    Connect: Connected & Fetch repo details from Landing Page Successfully

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -8,24 +8,50 @@ import { FetchReposService } from '../services/fetch-repos.service';
 	styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
+
 	constructor(private repoService: FetchReposService, private router: Router){};
-	failsBasicValidn: boolean = false;
 	username: string = '';
+
+	// This flag checks whether username abides by Github's rules for username
+	failsBasicValidn: boolean = false;
+	// Following is the regex for above
 	validnregex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
-	// submit (event: any, form: NgForm) {
+	showLoadingIcon: boolean = false;
+	showUserNotFoundIcon: boolean = false;
+	userData: any;
+
 	submit (usernameField: HTMLInputElement) {
-
-		// this.username = usernameField.value;
-
-		// if(this.validnregex.test(this.username)){
-		// 	this.repoService.getUsername(this.username);
-		// 	this.failsBasicValidn = false;
-		// }
-		// else{
-		// 	usernameField.value = '';
-		// 	this.failsBasicValidn = true;
-		// }
-
-		this.router.navigate(['/profile'])
+		this.showUserNotFoundIcon = false;
+		this.failsBasicValidn = false;
+		
+		this.username = usernameField.value;
+		this.process_username(usernameField)
+			.then(data => console.log(data))
+			.catch(error => console.log(error.message));
+		
  	}
+
+	process_username = async (usernameField: HTMLInputElement) => {
+		if(this.validnregex.test(this.username)){
+			this.showLoadingIcon = true;
+			let response: any = await this.repoService.getUserRepos(this.username);
+			this.userData = await response.json();
+			this.userData = JSON.parse(this.userData);
+			if(this.userData.message){
+				this.showLoadingIcon = false;
+				this.showUserNotFoundIcon = true;
+				return;
+			}
+			else{
+				this.showLoadingIcon = false;
+				return this.userData;
+			}
+			// this.router.navigate(['/profile'])
+		}
+		else{
+			this.showUserNotFoundIcon = false;
+			this.failsBasicValidn = true;
+			usernameField.value = '';
+		}
+	}
 }

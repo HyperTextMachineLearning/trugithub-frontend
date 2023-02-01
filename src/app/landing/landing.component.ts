dd744f478bf3e6edcf767cb5f3bfa94ba381bc0e
Ritik Matere <dev.ritik.matere@gmail.com>
2023-02-01 15:08:57 +0530

    Remove: Removed search's code from LandingComponent

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { FetchReposService } from '../services/fetch-repos.service';
 
 @Component({
 	selector: 'app-landing',
@@ -9,60 +7,4 @@ import { FetchReposService } from '../services/fetch-repos.service';
 })
 export class LandingComponent {
 
-	constructor(
-		private repoService: FetchReposService, 
-		private router: Router,
-		){};
-	username: string = '';
-
-	// This flag checks whether username abides by Github's rules for username
-	failsBasicValidn: boolean = false;
-	// Following is the regex for above
-	validnregex = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i;
-	showLoadingIcon: boolean = false;
-	showUserNotFoundIcon: boolean = false;
-	userData: any;
-
-	submit (usernameField: HTMLInputElement) {
-		this.showUserNotFoundIcon = false;
-		this.failsBasicValidn = false;
-		
-		this.username = usernameField.value;
-		this.process_username(usernameField)
-			.then(data => {
-				if (data == 'not found' || data == 'invalid') {
-
-				}
-				else {
-					this.repoService.setUserData(data);
-					this.router.navigate(['/profile'])
-				}
-			})
-			.catch(error => console.log(error.message));
-		
- 	}
-
-	process_username = async (usernameField: HTMLInputElement) => {
-		if(this.validnregex.test(this.username)){
-			this.showLoadingIcon = true;
-			let response: any = await this.repoService.getUserRepos(this.username);
-			this.userData = await response.json();
-			this.userData = JSON.parse(this.userData);
-			if(this.userData.message){
-				this.showLoadingIcon = false;
-				this.showUserNotFoundIcon = true;
-				return 'not found';
-			}
-			else{
-				this.showLoadingIcon = false;
-				return this.userData;
-			}
-		}
-		else{
-			this.showUserNotFoundIcon = false;
-			this.failsBasicValidn = true;
-			usernameField.value = '';
-			return 'invalid';
-		}
-	}
 }
